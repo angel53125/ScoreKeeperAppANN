97f92330600c079a1ef2e7f3e39318fbd902195a
pranshugupta123 <pranshu.gupta@desales.edu>
2022-10-13 10:45:47 -0400

    test

--- app/src/main/java/com/hfad/scorekeeperappann/MainActivity.java
+++ app/src/main/java/com/hfad/scorekeeperappann/MainActivity.java
@@ -9,8 +9,8 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int left_Score = 0;
-    private int right_Score = 0;
+    private int tv_left_Score = 0;
+    private int tv_right_Score = 0;
     private boolean running = false;
     public static final String LEFT_SCORE = "leftScore";
     public static final String RIGHT_SCORE = "rightScore";
@@ -22,26 +22,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnLeft = (Button) findViewById(R.id.button2);
-        Button btnRight = (Button) findViewById(R.id.button);
-        TextView tvLeft = (TextView) findViewById(R.id.textView3);
-        TextView tvRight = (TextView) findViewById(R.id.textView4);
+        Button btnLeft = (Button) findViewById(R.id.buttonLeft);
+        Button btnRight = (Button) findViewById(R.id.buttonRight);
+        TextView tvRight = (TextView) findViewById(R.id.textViewRight);
+        TextView tvLeft = (TextView) findViewById(R.id.textViewLeft);
 
 
         if (savedInstanceState != null)
         {
-
-//Restore the values from the bundle for offset/running
-            left_Score = savedInstanceState.getInt(LEFT_SCORE);
-            right_Score = savedInstanceState.getInt(RIGHT_SCORE);
+            //Restore the values from the bundle for offset/running
+            tv_left_Score = savedInstanceState.getInt(LEFT_SCORE);
+            tv_right_Score = savedInstanceState.getInt(RIGHT_SCORE);
             running = savedInstanceState.getBoolean(RUNNING_KEY);
-
-//If the stopwatch is in the middle of running,
-// we need to directly set the base time to whatever it was
-// at the start of the run and start the clock.
-//Otherwise, we can just call the setBaseTime() method like we usually do
+            tvLeft.setText(Integer.toString(tv_left_Score));
+            tvRight.setText(Integer.toString(tv_right_Score));
         }
-            
+
 
 
 
@@ -50,38 +46,32 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                left_Score++;
+                tv_left_Score++;
 
-                System.out.println(left_Score);
-                tvLeft.setText(Integer.toString(left_Score));
+                System.out.println(tv_left_Score);
+                tvLeft.setText(Integer.toString(tv_left_Score));
             }
         });
 
         btnRight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                right_Score++;
-
-                System.out.println(right_Score);
-                tvRight.setText(Integer.toString(right_Score));
-
+                tv_right_Score++;
+                System.out.println(tv_right_Score);
+                tvRight.setText(Integer.toString(tv_right_Score));
             }
         });
-
-
-
     }
 
 
     @Override
     protected void onSaveInstanceState(Bundle savedInstanceState)
     {
-//call the parent method and let it do its thing.
-//I get a warning if I do not do this.
         super.onSaveInstanceState(savedInstanceState);
+
 //Save the state of our activities' special properties
-        savedInstanceState.putInt(LEFT_SCORE, left_Score);
-        savedInstanceState.putInt(RIGHT_SCORE, right_Score);
+        savedInstanceState.putInt(LEFT_SCORE, tv_left_Score);
+        savedInstanceState.putInt(RIGHT_SCORE, tv_right_Score);
         savedInstanceState.putBoolean(RUNNING_KEY, running);
     }
 
